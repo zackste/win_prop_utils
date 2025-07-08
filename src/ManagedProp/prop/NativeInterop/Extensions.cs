a8f00359d38a5d32cd015b756f7e6ee4a70458df
Zack Stephens <4434856+zackste@users.noreply.github.com>
2025-07-08 06:24:46 -0700

    More interesting example

---
+++ src/ManagedProp/prop/NativeInterop/Extensions.cs
@@ -0,0 +1,30 @@
+﻿using prop.NativeInterop.Types;
+using System.Runtime.InteropServices;
+
+namespace prop.NativeInterop;
+
+using HRESULT = int;
+
+public enum DisplayNameFallback
+{
+    Null = 0,
+    Canonical
+}
+
+public static class Extensions
+{
+    // https://learn.microsoft.com/en-us/windows/win32/seccrypto/common-hresult-values
+    private const HRESULT E_FAIL = unchecked((int)0x80004005);
+
+    public static string? GetDisplayName(this IPropertyDescription propertyDescription, DisplayNameFallback fallback)
+    {
+        try
+        {
+            return propertyDescription.GetDisplayName();
+        }
+        catch (COMException come) when (come.HResult == E_FAIL)
+        {
+            return fallback == DisplayNameFallback.Null ? (string?)null : propertyDescription.GetCanonicalName();
+        }
+    }
+}
